afbf5214ca462764ee89c0b637cdd9022a1122e3
Isaac De La Lama <isaacdelalama1013@gmail.com>
2022-11-15 17:58:15 -0600

    created user model

--- flask_app/models.py
+++ flask_app/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # create db SQLAclchemy object
 db = SQLAlchemy()
@@ -7,4 +8,20 @@ db = SQLAlchemy()
 # create User class for users table and flask-login
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
-    raise NotImplementedError
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    
+    # to easily initialize a user object and hash the password
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        """
+        Checks if the password matches the users stored hashed password.
+        The password passed as an argument must be in plaintext. 
+        """
+        return check_password_hash(self.password, password)
