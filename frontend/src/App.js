c3a53e3f5f824c8f0c8bc9f939c5ebfe078511bb
Isaac De La Lama <isaacdelalama1013@gmail.com>
2022-11-15 18:47:49 -0600

    initial react commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react'
+import Login from './templates/Login'
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <div className='App'>
+      <Login/>
+    </div>
+  )
+}
+
+export default App
