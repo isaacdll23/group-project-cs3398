8633600e02459e2bac229beae7b2ce98829279f3
Isaac De La Lama <isaacdelalama1013@gmail.com>
2022-11-17 17:53:51 -0600

    created login page

--- flask_app/routes/auth.py
+++ flask_app/routes/auth.py
@@ -1,4 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 # create blueprint for the authentication routes of the site
 auth_blueprint = Blueprint('auth', __name__, url_prefix='/auth')
+
+@auth_blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        # POST method
+        raise NotImplementedError
--- flask_app/routes/main.py
+++ flask_app/routes/main.py
@@ -5,4 +5,4 @@ main_blueprint = Blueprint('main', __name__)
 
 @main_blueprint.route('/')
 def index():
-    return render_template()
+    return render_template('index.html')
