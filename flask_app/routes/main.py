751606ee7f2ccdf786d676cfdb7dcec73ec55d54
Isaac De La Lama <isaacdelalama1013@gmail.com>
2022-11-17 19:30:03 -0600

    added user registration and login backend and ui

--- flask_app/routes/main.py
+++ flask_app/routes/main.py
@@ -6,3 +6,7 @@ main_blueprint = Blueprint('main', __name__)
 @main_blueprint.route('/')
 def index():
     return render_template('index.html')
+
+@main_blueprint.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
