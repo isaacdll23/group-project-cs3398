8ee270a137a2a1e7b3fde84c95b9e429c717900a
Isaac De La Lama <isaacdelalama1013@gmail.com>
2022-11-24 18:29:38 -0600

    added functionality to save state of the button to the db per task

---
+++ flask_app/static/js/dashboard.js
@@ -0,0 +1,16 @@
+
+buttons = document.getElementsByClassName('form-check-input');
+
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', ()=> {
+        let data = {"task": buttons[i].value, "day": buttons[i].name};
+
+        fetch("/updateDailyTask", {
+            method: "PATCH",
+            headers: {'Content-Type': 'application/json'}, 
+            body: JSON.stringify(data)
+        }).then(res => {
+            console.log("Request complete! response:", res);
+        });
+    })
+}
