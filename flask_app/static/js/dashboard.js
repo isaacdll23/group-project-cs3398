f4febe82b1e2ec7543ed28c49a993f8477bf4991
Isaac De La Lama <isaacdelalama1013@gmail.com>
2022-11-24 20:23:13 -0600

    added weekly progress bar

--- flask_app/static/js/dashboard.js
+++ flask_app/static/js/dashboard.js
@@ -1,6 +1,10 @@
+window.onload = () => {
+    updateWeeklyProgressBar()
+    // TODO: Implement updateDailyProgressBar()
+}
 
+// add event listener to all the checkboxes to update with the database in real time
 buttons = document.getElementsByClassName('form-check-input');
-
 for(let i = 0; i < buttons.length; i++){
     buttons[i].addEventListener('click', ()=> {
         let data = {"task": buttons[i].value, "day": buttons[i].name};
@@ -12,5 +16,26 @@ for(let i = 0; i < buttons.length; i++){
         }).then(res => {
             console.log("Request complete. Response:", res);
         });
+
+
+
+        updateWeeklyProgressBar()
     })
+}
+
+// updates the width of the progress bar element
+function updateWeeklyProgressBar() {
+    var elem = document.getElementById("weeklyProgressBar");
+
+    let buttonsChecked = 0;
+    for(let j = 0; j < buttons.length; j++){
+        if (buttons[j].checked){
+            buttonsChecked++;
+        }
+    }
+
+    var new_width = Math.round(buttonsChecked / buttons.length * 100);
+
+    elem.style.width = new_width + "%";
+    elem.innerText = new_width + "%";
 }
