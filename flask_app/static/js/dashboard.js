505fb176016b973f3bf4731897343763cb6246cb
Garrett Cornett <gcornett92@gmail.com>
2022-11-27 16:39:31 -0600

    implemented daily progress bar

--- flask_app/static/js/dashboard.js
+++ flask_app/static/js/dashboard.js
@@ -1,6 +1,7 @@
 window.onload = () => {
     updateWeeklyProgressBar()
     // TODO: Implement updateDailyProgressBar()
+    updateDailyProgressBar() 
 }
 
 // add event listener to all the checkboxes to update with the database in real time
@@ -20,6 +21,7 @@ for(let i = 0; i < buttons.length; i++){
 
 
         updateWeeklyProgressBar()
+        updateDailyProgressBar() 
     })
 }
 
@@ -35,6 +37,32 @@ function updateWeeklyProgressBar() {
     }
 
     var new_width = Math.round(buttonsChecked / buttons.length * 100);
+
+    elem.style.width = new_width + "%";
+    elem.innerText = new_width + "%";
+}
+
+// updates the width of the progress bar element
+function updateDailyProgressBar() {
+    var elem = document.getElementById("dailyProgressBar");
+    const d = new Date();
+    let day = d.getDay(Date.now());
+    
+    if(day === 0){
+        day = 6;
+    }
+    else{
+        day -= 1;
+    }
+
+    let buttonsChecked = 0;
+    for(let j = day; j < buttons.length; j = j + 7){
+        if (buttons[j].checked){
+            buttonsChecked++;
+        }
+    }
+ 
+    var new_width = Math.round((buttonsChecked / (buttons.length / 7)) * 100);
 
     elem.style.width = new_width + "%";
     elem.innerText = new_width + "%";
